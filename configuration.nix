197523c109ec66b64f9bdd235b880d9b7043e6d1
ilovealienz <github@brightlittlestars.moe>
2026-06-21 21:40:41 +0100

    update

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@
     ./modules/unstable.nix
     ./modules/social.nix
     ./modules/dev.nix
-    ./hosts/pc-system.nix
+    # ./hosts/pc-system.nix
     # ./modules/amd.nix
     # ./modules/nvidia.nix
     # ./modules/intel.nix
