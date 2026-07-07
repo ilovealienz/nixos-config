06d7f75f05e3ff866049e532060d1260d2c6c58b
ilovealienz <github@brightlittlestars.moe>
2026-07-07 18:08:15 +0100

    update

--- home/shell.nix
+++ home/shell.nix
@@ -26,7 +26,7 @@
       }
     ];
     shellAliases = {
-      nxrebuild = "sudo git -C /etc/nixos add -f hardware-configuration.nix && sudo nixos-rebuild switch --flake /etc/nixos#\$(hostname)";
+      nxrebuild = "rm -f ~/.gtkrc-2.0 ~/.gtkrc-2.0.backup; sudo git -C /etc/nixos add -f hardware-configuration.nix && sudo nixos-rebuild switch --flake /etc/nixos#\$(hostname)";
       nxupdate = "cd /etc/nixos && sudo nix flake update && nxrebuild";
       nxpush = "GIT_SSH_COMMAND='ssh -i /home/pc/.ssh/id_ed25519' sudo -E git -C /etc/nixos add -f . && sudo git -C /etc/nixos rm --cached hardware-configuration.nix 2>/dev/null; sudo git -C /etc/nixos commit -m 'update' && GIT_SSH_COMMAND='ssh -i /home/pc/.ssh/id_ed25519' sudo -E git -C /etc/nixos push";
       nxclean = "sudo nix-collect-garbage";
--- modules/media.nix
+++ modules/media.nix
@@ -1,6 +1,7 @@
 { pkgs, pkgs-unstable, ... }: {
   environment.systemPackages = with pkgs; [
     mpv
+    gimp
     ffmpeg
     obs-studio
     yt-dlp
